5357872dd91cf97006bc89ec93468728a3c54b56
Stefan hr Berder <hr@bonz.org>
2014-09-03 20:35:39 +0200

    add epa tests from docs

---
+++ tests/test_epa.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+
+from aqi.algos.epa import AQI
+from aqi.constants import (POLLUTANT_PM25, POLLUTANT_PM10,
+                          POLLUTANT_O3_8H, POLLUTANT_O3_1H,
+                          POLLUTANT_CO, POLLUTANT_SO2, POLLUTANT_NO2)
+
+
+class TestEPA(unittest.TestCase):
+    """
+    Test the EPA algorithm based on examples rfom the EPA official doc.
+    """
+
+    def test_O3(self):
+        """Test Ozone AQI"""
+        myaqi = AQI()
+        self.assertEqual(
+            myaqi.iaqi(POLLUTANT_O3_8H, 0.08753333),
+            129)
+
+    def test_O3_PM25_CO(self):
+        "Test O3, PM2.5 and CO AQI"""
+        myaqi = AQI()
+        self.assertEqual(
+            myaqi.aqi([
+                (POLLUTANT_O3_8H, 0.077),
+                (POLLUTANT_PM25, 40.9),
+                (POLLUTANT_CO, 8.4)
+            ]),
+            104)
