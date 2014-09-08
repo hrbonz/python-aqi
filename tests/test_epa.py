306c90c229234764a368ba984c08c48399558dca
Stefan hr Berder <hr@bonz.org>
2014-09-08 13:55:26 +0200

    add tests based on EPA examples

--- tests/test_epa.py
+++ tests/test_epa.py
@@ -10,7 +10,7 @@ from aqi.constants import (POLLUTANT_PM25, POLLUTANT_PM10,
 
 class TestEPA(unittest.TestCase):
     """
-    Test the EPA algorithm based on examples rfom the EPA official doc.
+    Test the EPA algorithm based on examples from the EPA official doc.
     """
 
     def test_O3(self):
@@ -19,9 +19,15 @@ class TestEPA(unittest.TestCase):
         self.assertEqual(
             myaqi.iaqi(POLLUTANT_O3_8H, '0.08753333'),
             129)
+        self.assertEqual(
+            myaqi.iaqi(POLLUTANT_O3_1H, '0.162'),
+            147)
+        self.assertEqual(
+            myaqi.iaqi(POLLUTANT_O3_8H, '0.141'),
+            211)
 
     def test_O3_PM25_CO(self):
-        "Test O3, PM2.5 and CO AQI"""
+        """Test O3, PM2.5 and CO AQI"""
         myaqi = AQI()
         self.assertEqual(
             myaqi.aqi([
@@ -30,3 +36,10 @@ class TestEPA(unittest.TestCase):
                 (POLLUTANT_CO, '8.4')
             ]),
             104)
+
+    def test_blank_bp(self):
+        """Test blank breakpoints"""
+        myaqi = AQI()
+        self.assertEqual(
+            myaqi.iaqi(POLLUTANT_O3_8H, '0.087'),
+            129)
